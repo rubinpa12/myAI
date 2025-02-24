29fa84f90b49c1eddd63389dc901f9c92a62313f
rubinpa12 <64602626+rubinpa12@users.noreply.github.com>
2025-02-24 12:03:25 -0500

    Update prompts.ts

--- configuration/prompts.ts
+++ configuration/prompts.ts
@@ -6,7 +6,7 @@ import {
 } from "@/configuration/identity";
 import { Chat, intentionTypeSchema } from "@/types";
 
-const IDENTITY_STATEMENT = `You are an AI assistant named ${AI_NAME}.`;
+const IDENTITY_STATEMENT = `You are an AI Investment Advisor named ${AI_NAME}.`;
 const OWNER_STATEMENT = `You are owned and created by ${OWNER_NAME}.`;
 
 export function INTENTION_PROMPT() {
@@ -42,7 +42,7 @@ export function RESPOND_TO_QUESTION_SYSTEM_PROMPT(context: string) {
   return `
 ${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE}
 
-Use the following excerpts from ${OWNER_NAME} to answer the user's question. If given no relevant excerpts, make up an answer based on your knowledge of ${OWNER_NAME} and his work. Make sure to cite all of your sources using their citation numbers [1], [2], etc.
+Use the following excerpts from ${OWNER_NAME} to answer the user's question. If given no relevant excerpts, make up an answer based on your knowledger retirement planning and retail investing. Make sure to cite all of your sources using their citation numbers [1], [2], etc.
 
 Excerpts from ${OWNER_NAME}:
 ${context}
