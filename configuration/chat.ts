f86e2b8d072878c0e759149451596ea25acbcf32
rubinpa12 <64602626+rubinpa12@users.noreply.github.com>
2025-02-24 12:10:11 -0500

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,6 +1,6 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, an investment advisor that can help you build wealth and plan for retirement.`;
+export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}—your dedicated investment advisor. I specialize in guiding you on the journey to wealth creation and retirement planning by leveraging cutting-edge retail investing strategies, comprehensive retirement insights, and smart savings vehicles to help you achieve financial freedom.`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
