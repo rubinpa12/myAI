2b73f9a1c225e376ee9ddcffd3bda7b0787cefa3
rubinpa12 <64602626+rubinpa12@users.noreply.github.com>
2025-02-24 12:07:05 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,7 +1,7 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `TradeSage an AI Investment Advisor`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const MESSAGE_PLACEHOLDER: string = `Awaiting your input..`;
 export const FOOTER_MESSAGE: string = `Custom AI Agent Developed by Payton Rubin, Copyright (2025)`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Conversation`;
 export const PAGE_TITLE: string = `TradeSage`;
